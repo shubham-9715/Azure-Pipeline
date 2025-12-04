e7fbb147c3eaf2a83112a403ebe27ebf5a732db8
Shubham <shubhamkapare95@gmail.com>
2025-12-04 09:21:48 +0000

    chore: add pipeline

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "math_utils.h"
+
+int main() {
+    std::cout << "Result: " << add(5, 3) << std::endl;
+    return 0;
+}
---
+++ tests/test_math.cpp
@@ -0,0 +1,8 @@
+#include <cassert>
+#include "../include/math_utils.h"
+
+int main() {
+    assert(add(5, 5) == 10);
+    assert(add(1, 2) == 3);
+    return 0;
+}

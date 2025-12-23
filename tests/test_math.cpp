69959c386bda7c9febb8eea6588a3b2b9c8f5775
Shubham <shubhamkapare95@gmail.com>
2025-12-23 17:08:17 +0000

    chore: add debian package stage

--- tests/test_math.cpp
+++ tests/test_math.cpp
@@ -1,8 +1,13 @@
-#include <cassert>
-#include "../include/math_utils.h"
+#include "math_utils.h"
+#include <gtest/gtest.h>
 
-int main() {
-    assert(add(5, 5) == 10);
-    assert(add(1, 2) == 3);
-    return 0;
+TEST(AdditionTest, SimpleCases) {
+    EXPECT_EQ(add(5, 5), 10);
+    EXPECT_EQ(add(1, 2), 3);
+    EXPECT_EQ(add(-1, 1), 0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
